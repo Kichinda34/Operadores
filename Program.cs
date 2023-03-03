9ff805818ade99306913dc036f93169dd57a739c
Kichinda34 <leandroschneiderlima@gmail.com>
2023-03-03 15:09:19 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n1, n2;
+        Console.WriteLine("Insira dois numeros: ");
+        n1 = int.Parse(Console.ReadLine());
+        n2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("O valor da soma de: " + n1 +" + " + n2 + " = " +(n1 + n2));
+        Console.WriteLine("O valor da subtração de: " + n1 + " - " + n2 + " = " + (n1 - n2));
+        Console.WriteLine("O valor da multiplicação de: " + n1 + " * " + n2 + " = " + (n1 * n2));
+        
+        if (n1 == 0)
+        {
+            Console.WriteLine("Não é possível divisão por 0");
+        }
+        else
+        {
+            Console.WriteLine("O valor da divisão de: " + n1 + " / " + n2 + " = " + (n1 / n2));
+        }
+    }
+}
